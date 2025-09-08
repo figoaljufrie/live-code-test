b81796e82bcd4adc413a33042eba4bc26699165f
figoaljufrie <figoaldjoeffri@gmail.com>
2025-09-08 11:24:51 +0700

    live-code-test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,85 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-export default function Home() {
+/*pseudocode:
+1. 
+fetch random string dari API random string.
+
+2. Pakai Axios.get buat dapetin URL-nya.
+
+3. 
+
+3. useEffect dua, satu buat fetch data, satu lagi untuk itung per alfabet.
+
+4. Setelah dihitung, dijadiin object, terus disimpen di state.
+
+*/
+
+const apiRandomString =
+  "https://www.random.org/strings/?num=10&len=32&upperalpha=on&unique=off&format=plain";
+
+export default function liveCode() {
+  //ini buat nampung data dari API.
+  const [data, setData] = useState<string[]>([]);
+  const [count, setCount] = useState<Record<string, number>>({});
+
+  //useEffect pertama, buat fetch data dari API.
+  useEffect(() => {
+    async function fetchData() {
+      //fetch data:
+      const response = await axios.get(apiRandomString);
+      //di split dulu per baris, jadiin string.
+      const splitString = response.data.split("\n");
+      setData(splitString);
+    }
+    fetchData();
+  }, []);
+
+  //useEffect kedua, buat itung per alfabet.
+  useEffect(() => {
+    //ini akan bisa run kalo udah ada data.
+    if (data.length) {
+      //setelah di split, di gabungin supaya bisa dihitung per alfabet.
+      const combinedString: string = data.join("");
+      const count: Record<string, number> = {};
+
+      //looping per karakter
+      for (let ch of combinedString) {
+        //Jaddin uppercase supaya sama rata.
+        const upper = ch.toUpperCase();
+
+        //kasih kondisi kalo uppercase, dari alfabet, kalo selain itu (misalnya angka), skip.
+        if (upper >= "A" && upper <= "Z") {
+          //count, kalo 0 tambah 1, kalo udah ada, tambah 1 juga.
+          count[upper] = (count[upper] || 0) + 1;
+        }
+        const sortedObject = Object.fromEntries(
+          Object.entries(count).sort(([a], [b]) => a.localeCompare(b))
+        );
+        setCount(sortedObject);
+        console.log(setCount);
+      }
+    }
+  }, [data]);
+  console.log(data);
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex p-8 justify-center items-center">
+      <h1 className="text-2xl">LIVE CODE CHALLANGE!</h1>
+      <ul className="w-40 border rounded-md">
+        {Object.entries(count).map(([letters, n]) => (
+          <li
+            key={letters}
+            className={`flex justify-between px-3 py-1 ${
+              n % 2 === 0 ? "text-red-600" : "text-white"
+            }`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <span>{letters}</span>
+            <span>{n}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
